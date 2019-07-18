634c59258acb3e6404698c5d4fff62c8adc77419
Yuriy <georgheim@mail.ru>
2019-07-18 21:41:29 +0600

    first commit

---
+++ sfa/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sfa.Models;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+
+namespace sfa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string strpage = "sfa/_main")
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(strpage);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public static class HttpRequestExtensions
+    {
+        private const string RequestedWithHeader = "X-Requested-With";
+        private const string XmlHttpRequest = "XMLHttpRequest";
+
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            if (request.Headers != null)
+            {
+                return request.Headers[RequestedWithHeader] == XmlHttpRequest;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ sfa/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿function OnCompleteJs() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+}
+
+function OnSuccessJs() {
+    var urlMy = $(this).attr("data-myurl");
+    var titleMy = $(this).attr("data-mytitle");
+    var currentState = window.history.state;
+    document.title = titleMy;
+    window.history.pushState(currentState, titleMy, urlMy);
+    return false;
+}
